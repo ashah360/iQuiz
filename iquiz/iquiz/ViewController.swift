0a2dc6082faede06fd67da627b4cd5cdbbbfe421
skarahx <arman.shah0700@gmail.comgit>
2018-10-29 23:50:40 -0700

    initial

---
+++ iquiz/iquiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iquiz
+//
+//  Created by Arman Shah on 10/29/18.
+//  Copyright © 2018 Arman Shah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
