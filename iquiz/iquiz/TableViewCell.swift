c19a4bad934533ba4dbc8ec4f566ead04f2f66fb
skarahx <arman.shah0700@gmail.comgit>
2018-10-30 04:29:27 -0700

    fin

---
+++ iquiz/iquiz/TableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TableViewCell.swift
+//  iquiz
+//
+//  Created by Arman Shah on 10/30/18.
+//  Copyright © 2018 Arman Shah. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var subject: UILabel!
+    
+    
+    @IBOutlet weak var subjectDesc: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- iquiz/iquiz/ViewController.swift
+++ iquiz/iquiz/ViewController.swift
@@ -8,18 +8,55 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    var quizSubjects = ["Sports", "Games", "Music"]
+    var subjectDescs = ["Lets get sweaty", "Video Games", "Feel the melody"]
+    var imgs = ["sports", "games", "music"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "reuseCell", for: indexPath) as! TableViewCell
+        cell.subject.text = self.quizSubjects[indexPath.row]
+        cell.subjectDesc.text = self.subjectDescs[indexPath.row]
+        cell.imageView?.image = UIImage(named: self.imgs[indexPath.row])
+        cell.imageView?.contentMode = .scaleAspectFit
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return quizSubjects.count //3
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    
+    @IBAction func clickSettings(_ sender: Any) {
+        let alert = UIAlertController(title: "Settings", message: "Coming soon!", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK",
+                                      style: .default,
+                                      handler: { _ in
+                                        NSLog("OK button was pressed")
+        }))
+        self.present(alert, animated: true, completion: {
+            NSLog("The completion handler fired")
+        })
+    }
+    
+    
 }
 
